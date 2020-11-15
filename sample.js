66e42093b25718437c00feb74436ffcf633a8703
swarupa1718 <swarupam1718@gmail.com>
2020-11-15 10:57:45 +0100

    adding new javascript file

---
+++ sample.js
@@ -0,0 +1,34 @@
+<!--HTML Code-->
+<!-- Slideshow Container Div -->
+<div class="container"> 
+  
+  <!-- Full-width images with caption text -->
+  <div class="image-sliderfade fade"> 
+    <img src="img1.jpg" style="width:100%"> 
+    <div class="text">Image caption 1</div> 
+  </div> 
+  
+  <div class="image-sliderfade fade"> 
+    <img src="img2.jpg" style="width:100%"> 
+    <div class="text">Image caption 2</div> 
+  </div> 
+  
+  <div class="image-sliderfade fade"> 
+    <img src="img3.jpg" style="width:100%"> 
+    <div class="text">Image caption 3</div> 
+  </div> 
+  
+  <div class="image-sliderfade fade"> 
+    <img src="img3.jpg" style="width:100%"> 
+    <div class="text">Image caption 4</div> 
+  </div> 
+  
+</div> 
+<br> 
+  
+<!-- The dots/circles -->
+<div style="text-align:center"> 
+  <span class="dot"></span>  
+  <span class="dot"></span>  
+  <span class="dot"></span>  
+</div>
